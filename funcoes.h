1f4944164972369815fe970e622b2d845b3981ba
castilh0s <henriquedecastilhos@gmail.com>
2018-06-29 19:58:11 -0300

    agora sim

--- funcoes.h
+++ funcoes.h
@@ -6,7 +6,6 @@
   #include <ctype.h>
   #include <windows.h>
 
-  #define clear() printf("\033[H\033[J")
   #define MAX_AGENDA 50
   #define LINHAS 24
   #define COLUNAS 14
@@ -19,19 +18,12 @@
 
   void imprimeMenu();
   void processaOpcao(int opcao, registro r[], int *tamanho);
-
   void criaContato(registro r[], int *tamanho);
-
   void removeContato(registro r[], int *tamanho);
-
   void pesquisaContato(registro r[], int *tamanho);
-
   void listaContatos(registro r[], int *tamanho);
-
   void editaContato(registro r[], int *tamanho);
-
   void gotoxy(short col, short row);
-
   void desenharTela(short altura, short largura, short x, short y);
   void desenharLinha(short linha, short largura, short x, short y);
   void desenharTelaComTitulo(short altura, short largura, short x, short y, char* titulo);
