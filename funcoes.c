10a9e8be38decdde802cabe821a29c0904227b5a
castilh0s <henriquedecastilhos@gmail.com>
2018-06-29 00:30:37 -0300

    funcoes

--- funcoes.c
+++ funcoes.c
@@ -7,38 +7,6 @@ char _conteudo[LINHAS][COLUNAS] = {
   {200, 205, 188}
 };
 
-void imprimeMenu() {
-  system("cls");
-  desenharTelaComTitulo(10, 50, 5, 5, "Agenda Eletronica");
-
-  gotoxy(8, 9);
-  printf("1 - Criar Contato");
-  gotoxy(8, 10);
-  printf("2 - Remover Contato");
-  gotoxy(8, 11);
-  printf("3 - Pesquisar Contato");
-  gotoxy(8, 12);
-  printf("4 - Listar Contato");
-  gotoxy(8, 13);
-  printf("5 - Editar Contato");
-  gotoxy(8, 14);
-  printf("S - Sair");
-
-  gotoxy(7, 16);
-	printf("[   ] Escolha uma opcao");
-}
-
-void listaContatos(registro r[], int *tamanho) {
-	system("cls");
-	int tam = *tamanho;
-	printf("Lista de Contatos\n");
-	printf("Nome\tIdade\n");
-
-	for(int i = 0; i < tam; i++){
-		printf("%s\t%d\n", r[i].nome, r[i].idade);
-	}
-}
-
 void processaOpcao(int opcao, registro r[], int *tamanho) {
 	switch(opcao) {
 		case '1':
@@ -62,18 +30,58 @@ void processaOpcao(int opcao, registro r[], int *tamanho) {
 	}
 }
 
+void imprimeMenu() {
+  system("cls");
+  desenharTelaComTitulo(10, 50, 5, 5, "Agenda Eletronica");
+
+  gotoxy(8, 9);
+  printf("1 - Criar Contato");
+  gotoxy(8, 10);
+  printf("2 - Remover Contato");
+  gotoxy(8, 11);
+  printf("3 - Pesquisar Contato");
+  gotoxy(8, 12);
+  printf("4 - Listar Contato");
+  gotoxy(8, 13);
+  printf("5 - Editar Contato");
+  gotoxy(8, 14);
+  printf("S - Sair");
+
+  gotoxy(7, 16);
+	printf("[   ] Escolha uma opcao");
+}
+
 void criaContato(registro r[], int *tamanho) {
   int indice = *tamanho;
 
   system("cls");
   desenharTelaComTitulo(10, 50, 5, 5, "Criar Contato");
 
-	printf("Entre o nome do contato: ");
-	scanf("%s", r[indice].nome);
+  gotoxy(8, 9);
+	printf("Nome: [                                ]");
+	gotoxy(8, 10);
+  printf("Fone: [             ]");
 
-	printf("Entre a idade do contato: ");
-	scanf("%d", &r[indice].idade);
+  gotoxy(16, 9);
+  scanf("%s", r[indice].nome);
+  gotoxy(16, 10);
+	scanf("%f", &r[indice].fone);
 	*tamanho = *tamanho + 1;
+}
+
+void listaContatos(registro r[], int *tamanho) {
+  int tam = *tamanho;
+
+  system("cls");
+	printf("Lista de Contatos\n");
+	printf("Nome\tFone\n");
+
+	for(int i = 0; i < tam; i++){
+		printf("%s\t%.0f\n", r[i].nome, r[i].fone);
+	}
+
+  printf("\nAperte Enter para continuar...");
+  getch();
 }
 
 void desenharTelaComTitulo(short altura, short largura, short x, short y, char* titulo) {
