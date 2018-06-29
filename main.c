01fd504be0e56fe738a50ce42968123e4dd7d99e
castilh0s <henriquedecastilhos@gmail.com>
2018-06-28 23:54:43 -0300

    main

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "funcoes.h"
+
+int main() {
+	FILE *arquivo;
+	char *nome_arquivo = "agenda.bin";
+	char op;
+
+	registro agenda[MAX_AGENDA];
+
+	int tamanho = 0;
+	// lê o arquivo e carrega seu conteúdo para o vetor "agenda", se existir
+	arquivo = fopen(nome_arquivo, "rb");
+	if (arquivo != NULL) {
+		fread(&tamanho, sizeof(int), 1, arquivo);
+		fread(agenda, sizeof(registro), MAX_AGENDA, arquivo);
+		fclose(arquivo);
+	} else {
+		printf("Sem arquivo!");
+	}
+
+	do {
+		system("cls"); // limpa a tela - multiplataforma
+
+		printf("%d", tamanho);
+
+		imprimeMenu();
+		gotoxy(9, 16);
+		op = tolower(getchar());
+		processaOpcao(op, agenda, &tamanho);
+		fflush(stdin); // limpa o buffer do teclado (para remover lixos)
+		getchar();
+		getchar();
+	} while (op != 's');
+
+	// grava o arquivo, com o conteúdo do vetor, ao sair
+	arquivo = fopen(nome_arquivo, "wb");
+	fwrite(&tamanho, sizeof(int), 1, arquivo);
+	fwrite(agenda, sizeof(registro), MAX_AGENDA, arquivo);
+	fclose(arquivo);
+
+	return 0;
+}
